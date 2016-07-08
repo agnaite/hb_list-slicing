cca470c41ecf937788518dad46d363aa149b9b85
Hackbright Student <info@hackbrightacademy.com>
2016-07-08 12:24:26 -0700

    Finalized functions

--- list_operations.py
+++ list_operations.py
@@ -180,7 +180,8 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
+    input_list[2] = 37
+    input_list[-1] = 37
 
 
 def replace_middle(input_list):
@@ -197,7 +198,7 @@ def replace_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = [42, 37]
 
 
 def delete_third_and_seventh(input_list):
@@ -214,7 +215,8 @@ def delete_third_and_seventh(input_list):
 
     """
 
-    pass
+    del input_list[6]
+    del input_list[2]
 
 
 def delete_middle(input_list):
@@ -231,7 +233,7 @@ def delete_middle(input_list):
 
     """
 
-    pass
+    input_list[2:-2] = []
 
 
 ##############################################################################
